c2bba1d4d63948b3533e71640986651688213609
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-03-30 15:31:16 -0700

    fix(taskoverrides): override entrypoint to reset to system default

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ const main = async () => {
             {
               name: overrideContainer,
               command: overrideContainerCommand,
+              entryPoint: [""],
             },
           ],
         };
